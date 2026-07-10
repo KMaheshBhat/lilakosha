7c0e3ca0dacb8a3db45cf4ece1c32ebac152ee90
Mahesh K Bhat <k.mahesh.bhat@gmail.com>
2026-07-10 10:18:54 +0530

    feat: add report-record-health pipeline and update watch script

--- watch-cdm.sh
+++ watch-cdm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 watch -n 120 "./run.sh \
-    pipeline/35-report-records.yml \
+    pipeline/35-report-health.yml \
     --audit_only true \
     --hide_anomaly_details true \
     --report_breakdown true \
